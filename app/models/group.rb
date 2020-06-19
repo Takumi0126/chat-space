4c588298ee7a1606d330cbe0d4107c0d0ec97e56
Takumi0126 <65918404+Takumi0126@users.noreply.github.com>
2020-06-20 05:55:43 +0900

    view last_message

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if messages.last.present?
+      messages.last.content? ? messages.last.content : '画像が送信されています'
+    else
+      'まだメッセージはありません'
+    end
+  end
 end
